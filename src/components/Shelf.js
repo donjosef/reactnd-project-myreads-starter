d788921aa1976e84259e39b15009127ac33e9244
donjoe <giumontanaro@gmail.com>
2018-07-01 20:39:11 +0200

    change the shelf of the book when the select changes

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,17 +1,11 @@
 import React  from 'react'
 
 function Shelf(props) {
-
-    const books = props.books; //array
-    console.log(books);
-    books.forEach(book => {
-      book.shelf = book.shelf.split(/(?=[A-Z])/).join(" "); //making the title of shelf separated at every uppercase
-    }); 
+    const {books, updateShelf} = props; //array
     
-  
     return(
         <div className="bookshelf">
-                  <h2 className="bookshelf-title">{books.length > 0 ? books[0].shelf : ''}</h2>
+                  <h2 className="bookshelf-title">{books.length > 0 ? books[0].shelf : 'Empty Shelf'}</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       {books.map(book => (
@@ -24,7 +18,7 @@ function Shelf(props) {
                                     height: 193
                               }} className='book-cover' />
                               <div className='book-shelf-changer'>
-                                <select>
+                                <select defaultValue={book.shelf} onChange={(e) => updateShelf(book.id, e.target.value)}>
                                   <option value="move" disabled>Move to...</option>
                                   <option value="currentlyReading">Currently Reading</option>
                                   <option value="wantToRead">Want to Read</option>
